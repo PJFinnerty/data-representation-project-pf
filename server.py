a43a3f7615776616d0465b2a9090ca3ce5ab6b7c
Peter Finnerty <finnertypete@gmail.com>
2022-12-12 21:27:33 +0000

    Create Cinema SQL table. Created Server host file, and input routing to basic functions in DAO. More work required on functions.

--- server.py
+++ server.py
@@ -1,1 +1,25 @@
 from flask import Flask, jsonify, request, abort
+from DAO import TestDAO
+
+app = Flask(__name__, static_url_path='', static_folder='.')
+
+@app.route('/cinema')
+def findAll():
+    #print("in findAll")
+    results = TestDAO.findAll()
+    return jsonify(results)
+    
+@app.route('/cinema/<int:id>')
+def findItem(id):
+    foundItem = TestDAO.findItem(id)
+    return jsonify(foundItem)
+    
+@app.route('/delivery', methods=['POST'])
+def create():
+
+    delivery = {
+            "Film": request.json['Film'],
+            "Director": request.json['Director'],
+            "Screen": request.json['Screen'],
+            "Minutes": request.json['Minutes'],
+        }
