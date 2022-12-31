47d4fa3f35ffbb687ea2c9cac5bcc0eb0c3f93cf
Peter Finnerty <finnertypete@gmail.com>
2022-12-31 19:00:39 +0000

    Updated server file to route to the 3 specific cinema delivery DAO files. Updated web_cinemas.html to function as homepage - created functions to redirect user to specific delivery page after clicking on the relevant cinema, function redirectHeadford(), function redirectOranmore() & function redirectEyeCinema().

--- server_choose_film.py
+++ server_choose_film.py
@@ -51,8 +51,7 @@ def getToOrder():
     #return '{"data":"all here"}'
     return redirect('web_cinemas5.html')
 #==============================================
-
-# Cinema 
+# Main Cinema Page 
 @app.route('/cinemas')
 def findAllCinemas():
     #print("in findAllCinemas")
@@ -62,27 +61,27 @@ def findAllCinemas():
 @app.route('/cinemas/<int:id>')
 def findCinema_Name(id):
     foundCinema_Name = cinemasDAO.findCinema_Name(id)
-    return jsonify(foundCinema_Name)
+    return jsonify(foundType)
 
 @app.route('/cinemas', methods=['POST'])
 def createCinemas():  
     if not request.json:
         abort(400)
     cinemas = {
-        "Cinema_Name": request.json['Cinema_Name'],
+        "Type": request.json['Type'],
         "Location": request.json['Location'],
         "NumberOfScreens": request.json['NumberOfScreens'],
         "Member_Status": request.json['Member_Status'],
     }
-    values =(cinemas['Cinema_Name'],cinemas['Location'],cinemas['NumberOfScreens'], cinemas['Member_Status'])
+    values =(cinemas['Type'],cinemas['Location'],cinemas['NumberOfScreens'], cinemas['Member_Status'])
     newId = cinemasDAO.createCinemas(values)
     cinemas['id'] = newId
     return jsonify(cinemas)
 
 @app.route('/cinemas/<int:id>', methods=['PUT'])
 def updateCinemas(id):
-    foundCinema_Name = cinemasDAO.findCinema_Name(id)
-    if not foundCinema_Name:
+    foundType = cinemasDAO.findCinema_Name(id)
+    if not foundType:
         abort(404)   
     if not request.json:
         abort(400)
@@ -91,25 +90,200 @@ def updateCinemas(id):
         abort(400)
     if 'NumberOfScreens' in reqJson and Location(reqJson['NumberOfScreens']) is not int:
         abort(400)
-    if 'Cinema_Name' in reqJson:
-        foundCinema_Name['Cinema_Name'] = reqJson['Cinema_Name']
+    if 'Type' in reqJson:
+        foundType['Type'] = reqJson['Type']
     if 'Location' in reqJson:
-        foundCinema_Name['Location'] = reqJson['Location']
+        foundType['Location'] = reqJson['Location']
     if 'NumberOfScreens' in reqJson:
-        foundCinema_Name['NumberOfScreens'] = reqJson['NumberOfScreens']
+        foundType['NumberOfScreens'] = reqJson['NumberOfScreens']
     if 'Member_Status' in reqJson:
-        foundCinema_Name['Member_Status'] = reqJson['Member_Status']
-    values = (foundCinema_Name['Cinema_Name'],foundCinema_Name['Location'],foundCinema_Name['NumberOfScreens'],foundCinema_Name['Member_Status'],foundCinema_Name['id'])
+        foundType['Member_Status'] = reqJson['Member_Status']
+    values = (foundType['Type'],foundType['Location'],foundType['NumberOfScreens'],foundType['Member_Status'],foundType['id'])
     cinemasDAO.updateCinemas(values)
-    return jsonify(foundCinema_Name)
+    return jsonify(foundType)
         
 @app.route('/cinemas/<int:id>' , methods=['deleteCinemas'])
 def deleteCinemas(id):
     cinemasDAO.deleteCinemas(id)
     return jsonify({"done":True})
+ 
+#=============================================== 
+ 
+# Eye Cinema Connections
 
+@app.route('/EyeCinema')
+def findAll():
+    #print("in findAll")
+    results = deliveryDAO.findAll()
+    return jsonify(results)
+
+@app.route('/EyeCinema/<int:id>')
+def findItem(id):
+    foundItem = deliveryDAO.findItem(id)
+    return jsonify(foundItem)
+
+@app.route('/EyeCinema', methods=['POST'])
+def create():  
+    if not request.json:
+        abort(400)
+    delivery = {
+        "Item": request.json['Item'],
+        "Type": request.json['Type'],
+        "Quantity": request.json['Quantity'],
+        "TotPrice": request.json['TotPrice'],
+    }
+    values =(delivery['Item'],delivery['Type'],delivery['Quantity'], delivery['TotPrice'])
+    newId = deliveryDAO.create(values)
+    delivery['id'] = newId
+    return jsonify(delivery)
+
+@app.route('/EyeCinema/<int:id>', methods=['PUT'])
+def update(id):
+    foundItem = deliveryDAO.findItem(id)
+    if not foundItem:
+        abort(404)   
+    if not request.json:
+        abort(400)
+    reqJson = request.json
+    if 'TotPrice' in reqJson and type(reqJson['TotPrice']) is not int:
+        abort(400)
+    if 'Quantity' in reqJson and type(reqJson['Quantity']) is not int:
+        abort(400)
+    if 'Item' in reqJson:
+        foundItem['Item'] = reqJson['Item']
+    if 'Type' in reqJson:
+        foundItem['Type'] = reqJson['Type']
+    if 'Quantity' in reqJson:
+        foundItem['Quantity'] = reqJson['Quantity']
+    if 'TotPrice' in reqJson:
+        foundItem['TotPrice'] = reqJson['TotPrice']
+    values = (foundItem['Item'],foundItem['Type'],foundItem['Quantity'],foundItem['TotPrice'],foundItem['id'])
+    deliveryDAO.update(values)
+    return jsonify(foundItem)
+        
+@app.route('/EyeCinema/<int:id>' , methods=['DELETE'])
+def delete(id):
+    deliveryDAO.delete(id)
+    return jsonify({"done":True})
+
+#=============================================== 
+ # IMC Headford Road Connections
+
+# IMC Headford Road Connections
+@app.route('/IMCHeadfordRoad')
+def findAllHeadford():
+    #print("in findAll")
+    results = deliveryDAOHeadfordRoad.findAllHeadford()
+    return jsonify(results)
+
+@app.route('/IMCHeadfordRoad/<int:id>')
+def findItemHeadford(id):
+    foundItem = deliveryDAOHeadfordRoad.findItemHeadford(id)
+    return jsonify(foundItem)
+
+@app.route('/IMCHeadfordRoad', methods=['POST'])
+def createHeadford():  
+    if not request.json:
+        abort(400)
+    delivery = {
+        "Item": request.json['Item'],
+        "Type": request.json['Type'],
+        "Quantity": request.json['Quantity'],
+        "TotPrice": request.json['TotPrice'],
+    }
+    values =(delivery['Item'],delivery['Type'],delivery['Quantity'], delivery['TotPrice'])
+    newId = deliveryDAOHeadfordRoad.createHeadford(values)
+    delivery['id'] = newId
+    return jsonify(delivery)
+
+@app.route('/IMCHeadfordRoad/<int:id>', methods=['PUT'])
+def updateHeadford(id):
+    foundItem = deliveryDAOHeadfordRoad.findItemHeadford(id)
+    if not foundItem:
+        abort(404)   
+    if not request.json:
+        abort(400)
+    reqJson = request.json
+    if 'TotPrice' in reqJson and type(reqJson['TotPrice']) is not int:
+        abort(400)
+    if 'Quantity' in reqJson and type(reqJson['Quantity']) is not int:
+        abort(400)
+    if 'Item' in reqJson:
+        foundItem['Item'] = reqJson['Item']
+    if 'Type' in reqJson:
+        foundItem['Type'] = reqJson['Type']
+    if 'Quantity' in reqJson:
+        foundItem['Quantity'] = reqJson['Quantity']
+    if 'TotPrice' in reqJson:
+        foundItem['TotPrice'] = reqJson['TotPrice']
+    values = (foundItem['Item'],foundItem['Type'],foundItem['Quantity'],foundItem['TotPrice'],foundItem['id'])
+    deliveryDAOHeadfordRoad.updateHeadford(values)
+    return jsonify(foundItem)
+        
+@app.route('/IMCHeadfordRoad/<int:id>' , methods=['DELETE'])
+def deleteHeadford(id):
+    deliveryDAOHeadfordRoad.deleteHeadford(id)
+    return jsonify({"done":True})
+    
+#=============================================== 
+ # IMC Oranmore  Connections
+
+# IMC Oranmore  Connections
+@app.route('/IMCOranmore')
+def findAllOranmore():
+    #print("in findAll")
+    results = deliveryDAOOranmore.findAllOranmore()
+    return jsonify(results)
+
+@app.route('/IMCOranmore/<int:id>')
+def findItemOranmore(id):
+    foundItem = deliveryDAOOranmore.findItemOranmore(id)
+    return jsonify(foundItem)
+
+@app.route('/IMCOranmore', methods=['POST'])
+def createOranmore():  
+    if not request.json:
+        abort(400)
+    delivery = {
+        "Item": request.json['Item'],
+        "Type": request.json['Type'],
+        "Quantity": request.json['Quantity'],
+        "TotPrice": request.json['TotPrice'],
+    }
+    values =(delivery['Item'],delivery['Type'],delivery['Quantity'], delivery['TotPrice'])
+    newId = deliveryDAOOranmore.createOranmore(values)
+    delivery['id'] = newId
+    return jsonify(delivery)
+
+@app.route('/IMCOranmore/<int:id>', methods=['PUT'])
+def updateOranmore(id):
+    foundItem = deliveryDAOOranmore.findItemOranmore(id)
+    if not foundItem:
+        abort(404)   
+    if not request.json:
+        abort(400)
+    reqJson = request.json
+    if 'TotPrice' in reqJson and type(reqJson['TotPrice']) is not int:
+        abort(400)
+    if 'Quantity' in reqJson and type(reqJson['Quantity']) is not int:
+        abort(400)
+    if 'Item' in reqJson:
+        foundItem['Item'] = reqJson['Item']
+    if 'Type' in reqJson:
+        foundItem['Type'] = reqJson['Type']
+    if 'Quantity' in reqJson:
+        foundItem['Quantity'] = reqJson['Quantity']
+    if 'TotPrice' in reqJson:
+        foundItem['TotPrice'] = reqJson['TotPrice']
+    values = (foundItem['Item'],foundItem['Type'],foundItem['Quantity'],foundItem['TotPrice'],foundItem['id'])
+    deliveryDAOOranmore.updateOranmore(values)
+    return jsonify(foundItem)
+        
+@app.route('/IMCOranmore/<int:id>' , methods=['DELETE'])
+def deleteOranmore(id):
+    deliveryDAOOranmore.deleteOranmore(id)
+    return jsonify({"done":True})
+    
 
 if __name__ == '__main__' :
     app.run(debug= True)
-    
-      
